218e46ee765ecd62fda8be9520510716bba0a105
jws3873 <jws3873@gamil.com>
2024-07-28 03:44:27 +0900

    feat : 스프링데이터 JPA분석

--- src/main/java/study/datajpa/contoller/MemberController.java
+++ src/main/java/study/datajpa/contoller/MemberController.java
@@ -36,7 +36,7 @@ public class MemberController {
         return page;
     }
 
-    @PostConstruct
+//    @PostConstruct
     public void init(){
         for(int i =0; i<100; i++){
             memberRepository.save(new Member("user" + i, i));
